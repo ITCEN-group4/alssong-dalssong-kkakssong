a258c6780e4128d75600956b670afe093433d64a
mirageoasis <kimhw0820@naver.com>
2025-05-27 02:05:19 +0900

    feat: 로그인 어노테이션 추가

--- back/src/main/java/com/group4/aldalka/domain/common/LoginUser.java
+++ back/src/main/java/com/group4/aldalka/domain/common/LoginUser.java
@@ -1,2 +1,10 @@
-package com.group4.aldalka.domain.common;public @interface LoginUser {
-}
+package com.group4.aldalka.domain.common;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LoginUser {}
