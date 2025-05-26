08350bee812489ced781b3f7d9468573be3c3d9d
oh euna <erropp89@naver.com>
2025-05-26 09:30:24 +0900

    #3 feat: 스프링부트 환경세팅팅

---
+++ back/aldalka/src/test/java/com/group4/aldalka/AldalkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group4.aldalka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AldalkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
