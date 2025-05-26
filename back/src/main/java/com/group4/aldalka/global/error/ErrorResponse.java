b9f12ecb0d2ec28301f5e3590760cda3b1597e96
oh euna <erropp89@naver.com>
2025-05-26 16:09:35 +0900

    #3 fix: ErrorResponse파일 import 경로 수정

--- back/src/main/java/com/group4/aldalka/global/error/ErrorResponse.java
+++ back/src/main/java/com/group4/aldalka/global/error/ErrorResponse.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
-import javax.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolation;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
