1c7ed47533cc6ab6320bf36dfa9a52c377e2b9f9
happygirlll <lbm000314@gmail.com>
2025-06-01 09:38:42 +0900

    feat: 비밀번호 보기 여부 코드 추가

--- front/src/pages/LoginForm.jsx
+++ front/src/pages/LoginForm.jsx
@@ -1,36 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './Login.module.css';
 import email from '../assets/email.svg';
 import password from '../assets/password.svg';
 import login from '../assets/login.svg';
 import password_viewless from '../assets/password_viewless.svg';
+import password_visible from '../assets/password_visible.svg';
 
 export default function LoginForm() {
-    return (
-        <div className={styles.formWrapper}>
+
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(prev => !prev);
+    }
+
+    return (<div className={styles.formWrapper}>
             <div className={styles.login_title_form}>
-                <img src={login} alt="login_img" className={styles.login_img} />
+                <img src={login} alt="login_img" className={styles.login_img}/>
                 <h2 className={styles.formTitle}>로그인</h2>
             </div>
             <form className={styles.form}>
                 <label>
                     <span className={styles.input_label}>이메일</span>
                     <div className={styles.input_wrapper}>
-                        <img src={email} alt="email_img" className={styles.leftIcon} />
-                        <input type="email" placeholder="이메일을 입력하세요" className={styles.input} />
+                        <img src={email} alt="email_img" className={styles.leftIcon}/>
+                        <input type="email" placeholder="이메일을 입력하세요" className={styles.input}/>
                     </div>
                 </label>
                 <label>
                     <span className={styles.input_label}>비밀번호</span>
                     <div className={styles.input_wrapper}>
-                        <img src={password} alt="password_img" className={styles.leftIcon} />
-                        <input type="password" placeholder="비밀번호를 입력해주세요" className={styles.input} />
-                        <img src={password_viewless} alt="password_viewless_img" className={styles.rightIcon} />
+                        <img src={password} alt="password_img" className={styles.leftIcon}/>
+                        <input
+                            type={showPassword ? 'text' : 'password'}
+                            placeholder="비밀번호를 입력해주세요"
+                            className={styles.input}
+                        />
+                        <img
+                            src={showPassword ? password_visible : password_viewless}
+                            alt="password_viewless_img"
+                            className={styles.rightIcon}
+                            onClick={togglePasswordVisibility}
+                        />
                     </div>
                 </label>
                 <button type="submit" className={styles.loginButton}>로그인</button>
                 <div className={styles.registerLink}>회원가입</div>
             </form>
-        </div>
-    );
+        </div>);
 }
