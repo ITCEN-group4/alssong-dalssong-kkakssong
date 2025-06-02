383188adbbf65d4fb7c23b84ba61bfd1c011b335
Jordan <jordandan216@gmail.com>
2025-06-02 15:33:14 +0900

    feat: 검색창에 클릭 가능한 돋보기 버튼 추가

--- front/src/components/layout/SearchWriteBar.jsx
+++ front/src/components/layout/SearchWriteBar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./SearchWriteBar.module.css";
+import searchIcon from "../../assets/search.svg";
 
 export default function SearchWriteBar({ searchKeyword, setSearchKeyword ,onSearch}) {
     const navigate = useNavigate();
@@ -8,18 +9,26 @@ export default function SearchWriteBar({ searchKeyword, setSearchKeyword ,onSear
 
     return (
         <div className={styles.barContainer}>
-            <input
-                type="text"
-                value={searchKeyword}
-                onChange={(e) => setSearchKeyword(e.target.value)}
-                onKeyDown={(e) => {
-                    if (e.key === "Enter")
-                        onSearch();
-                }}
-                placeholder="칵테일을 검색하세요..."
-                className={styles.searchInput}
-            />
-            <button className={styles.writeButton} onClick={() => navigate("/write")}>
+            <div className={styles.searchContainer}>
+                <input
+                    type="text"
+                    value={searchKeyword}
+                    onChange={(e) => setSearchKeyword(e.target.value)}
+                    onKeyDown={(e) => {
+                        if (e.key === "Enter") onSearch();
+                    }}
+                    placeholder="칵테일을 검색하세요..."
+                    className={styles.searchInput}
+                />
+                <button
+                    type="button"
+                    onClick={onSearch}
+                    className={styles.searchButton}
+                >
+                    <img src={searchIcon} alt="search" className={styles.searchIcon}/>
+                </button>
+            </div>
+            <button className={styles.writeButton} onClick={() => navigate("/post/userId")}>
                 + 글쓰기
             </button>
         </div>
