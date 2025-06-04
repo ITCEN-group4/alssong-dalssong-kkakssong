530f93e07add2f2c104e78b6c0d34f357ae5f527
oh euna <erropp89@naver.com>
2025-06-05 00:47:59 +0900

    #16 chore: 사용하지 않는 레파지토리 삭제

--- back/src/main/java/com/group4/aldalka/domain/post/repository/IngredientReposiroty.java
+++
@@ -1,7 +0,0 @@
-package com.group4.aldalka.domain.post.repository;
-
-import com.group4.aldalka.domain.post.entity.Ingredient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IngredientReposiroty extends JpaRepository<Ingredient, Long> {
-}
