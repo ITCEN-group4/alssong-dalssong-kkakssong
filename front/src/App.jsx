32f48a3c1d56ce798ee86f6ce8a15e8181977774
happygirlll <lbm000314@gmail.com>
2025-06-07 17:14:53 +0900

    feat: app.js에 포함되지 않는 페이지 라우터 주석 처리 및 공식 칵테일 페이지 연결

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Routes, Route, BrowserRouter} from "react-router-dom";
 import CocktailSharePage from "./pages/CocktailSharePage";
-import CocktailDetailPage from "./pages/CocktailDetailPage";
-import CocktailWritePage from "./pages/CocktailWritePage.jsx";
+// import CocktailDetailPage from "./pages/CocktailDetailPage";
+// import CocktailWritePage from "./pages/CocktailWritePage.jsx";
 import {CocktailProvider} from "./context/CocktailContext.jsx";
 import LoginPage from './pages/LoginPage.jsx';
 import SignupPage from './pages/SignupPage.jsx';
+import OfficialCocktailPage from "./pages/OfficialCocktailPage.jsx"
 
 function App() {
     return (
@@ -15,8 +16,9 @@ function App() {
                     <Route path="/auth/login" element={<LoginPage />} />
                     <Route path="/auth/signup" element={<SignupPage/>} />
                     <Route path="/post" element={<CocktailSharePage/>}/>
-                    <Route path="/post/:id" element={<CocktailDetailPage/>}/>
-                    <Route path="/post/userId" element={<CocktailWritePage/>}/>
+                    {/*<Route path="/post/:id" element={<CocktailDetailPage/>}/>*/}
+                    {/*<Route path="/post/userId" element={<CocktailWritePage/>}/>*/}
+                    <Route path="/posts" element={<OfficialCocktailPage/>}/>
                 </Routes>
             </CocktailProvider>
         </BrowserRouter>
