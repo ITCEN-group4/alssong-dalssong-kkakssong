b42470e68355d16ffe7f37e92dfc93f945beabce
Jordan <jordandan216@gmail.com>
2025-06-09 16:47:13 +0900

    refactor: 다른 페이지 주석처리 및 sharePage 코드 일부분 수정

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import {Routes, Route, BrowserRouter} from "react-router-dom"
+import {CocktailProvider} from "./context/CocktailContext.jsx";
 import ScrollToTop from "./utils/ScrollToTop.jsx";
 import CocktailSharePage from "./pages/CocktailSharePage";
 import CocktailDetailPage from "./pages/CocktailDetailPage";
 import CocktailWritePage from "./pages/CocktailWritePage.jsx";
-import {CocktailProvider} from "./context/CocktailContext.jsx";
 import LoginPage from './pages/LoginPage.jsx';
 import SignupPage from './pages/SignupPage.jsx';
+import MyPage from "./pages/MyPage.jsx";
+import MyInfoPage from "./pages/MyInfoPage.jsx";
+import MyPostViewPage from "./pages/MyPostViewPage.jsx";
 
 function App() {
     return (
@@ -18,7 +21,12 @@ function App() {
                     <Route path="/auth/signup" element={<SignupPage/>} />
                     <Route path="/post" element={<CocktailSharePage/>}/>
                     <Route path="/post/:id" element={<CocktailDetailPage/>}/>
-                    <Route path="/post/userId" element={<CocktailWritePage/>}/>
+                    {/*<Route path="/post/create" element={<CocktailWritePage mode="create" />} />*/}
+                    {/*<Route path="/post/update/:id" element={<CocktailWritePage mode="edit" />} />*/}
+                    {/*<Route path="/mypage" element={<MyPage />}>*/}
+                    {/*    <Route index element={<MyInfoPage />} />*/}
+                    {/*    <Route path="posts" element={<MyPostViewPage />} />*/}
+                    {/*</Route>*/}
                 </Routes>
             </CocktailProvider>
         </BrowserRouter>
