369f1a909a5eb90a1d9af7d558533ef115e69262
oh euna <erropp89@naver.com>
2025-06-09 23:23:33 +0900

    #46 refactor: 포스트의 좋아요 여부를 가져오는 로직 리팩토링

--- back/src/main/java/com/group4/aldalka/domain/post/service/PostService.java
+++ back/src/main/java/com/group4/aldalka/domain/post/service/PostService.java
@@ -10,7 +10,6 @@ import com.group4.aldalka.domain.post.dto.response.PostResponse;
 import com.group4.aldalka.domain.post.entity.Post;
 import com.group4.aldalka.domain.post.repository.PostRepository;
 import com.group4.aldalka.domain.post.repository.UserLikeRepository;
-import com.group4.aldalka.domain.user.repository.UserRepository;
 import com.group4.aldalka.domain.user.service.UserService;
 import com.group4.aldalka.global.error.ErrorCode;
 import com.group4.aldalka.global.error.exception.BusinessException;
@@ -27,7 +26,6 @@ import java.util.stream.Collectors;
 public class PostService {
 
     private final PostRepository postRepository;
-    private final UserRepository userRepository;
     private final UserLikeRepository userLikeRepository;
     private final UserService userService;
 
@@ -40,41 +38,26 @@ public class PostService {
         int totalPages = Math.max(1, (int) Math.ceil((double) result.getTotalElements() / pageSize));
 
         return PagedResponse.<PostResponse> builder()
-                .posts(getPostsWithLikeInfo(userId, result.getPosts()))
+                .posts(mapToPostResponses(userId, result.getPosts()))
                 .totalPages(totalPages)
                 .totalElements(result.getTotalElements())
                 .build();
 
     }
 
-    public List<PostResponse> getPostsWithLikeInfo(Long userId, List<Post> posts) {
-        List<Long> postIds = posts.stream().map(Post::getPostId).collect(Collectors.toList());
-
-        // 쿼리 1: 특정 유저가 좋아요 누른 포스트 목록
-        List<Long> likedPostIds = userId == null
-                ? Collections.emptyList() //userId가 null이면 isLiked는 항상 false임
-                : userLikeRepository.findLikedPostIdsByUserIdAndPostIds(userId, postIds);
-
-        Set<Long> likedSet = new HashSet<>(likedPostIds);
-
-        return mapToPostResponses(posts, likedSet);
-    }
-
-    private List<PostResponse> mapToPostResponses(List<Post> posts, Set<Long> likedSet) {
+    private List<PostResponse> mapToPostResponses(Long userId, List<Post> posts) {
         return posts.stream()
                 .map(post -> PostResponse.from(
                         post,
                         post.getLikes().size(),
-                        likedSet.contains(post.getPostId())
+                        userLikeRepository.existsByUserUserIdAndPostPostId(userId, post.getPostId())
                 ))
                 .collect(Collectors.toList());
     }
 
     public OfficialPostDetailResponse getOfficialPostDetail(String userEmail, Long postId) {
 
-        Long userId = userRepository.findByEmail(userEmail)
-                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_EXISTS))
-                .getUserId();
+        Long userId = userService.getUserIdByEmail(userEmail);
 
         Post post = postRepository.findById(postId)
                 .orElseThrow(() -> new BusinessException(ErrorCode.POST_NOT_FOUND));
@@ -118,34 +101,24 @@ public class PostService {
         Long userId = userService.getUserIdByEmail(userEmail);
 
         PostSearchResult posts = postRepository.findPostsByUserAndCondition(userId, mypagePostSearchRequest);
-        List<Long> likedPostIds = userLikeRepository.findLikedPostIdsByUserIdAndPostIds(
-                userId,
-                posts.getPosts().stream()
-                        .map(Post::getPostId)
-                        .collect(Collectors.toList())
-        );
 
-
-        Set<Long> likedPostIdSet = new HashSet<>(likedPostIds);
-
-        PostSearchResult result = postRepository.findPostsByUserAndCondition(userId, mypagePostSearchRequest);
         int pageSize = 8;
-        int totalPages = Math.max(1, (int) Math.ceil((double) result.getTotalElements() / pageSize));
+        int totalPages = Math.max(1, (int) Math.ceil((double) posts.getTotalElements() / pageSize));
 
         return PagedResponse.<MypagePostResponse>builder()
-                .totalElements(result.getTotalElements())
+                .totalElements(posts.getTotalElements())
                 .totalPages(totalPages)
-                .posts(mapToMypagePostResponses(posts.getPosts(), likedPostIdSet))
+                .posts(mapToMypagePostResponses(userId, posts.getPosts()))
                 .build();
     }
 
-    private List<MypagePostResponse> mapToMypagePostResponses(List<Post> posts, Set<Long> likedPostIdSet) {
+    private List<MypagePostResponse> mapToMypagePostResponses(Long userId, List<Post> posts) {
         return posts.stream()
                 .map(post -> MypagePostResponse.builder()
                         .postId(post.getPostId())
                         .title(post.getTitle())
                         .likeCount(post.getLikes().size())
-                        .isLiked(likedPostIdSet.contains(post.getPostId()))
+                        .isLiked(userLikeRepository.existsByUserUserIdAndPostPostId(userId, post.getPostId()))
                         .createdAt(post.getCreatedAt().toLocalDate())
                         .imageUrl(post.getImageUrl())
                         .build())
