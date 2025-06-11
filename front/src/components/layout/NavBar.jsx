e7eb4776af3f1a20aa73b203b27d72f9e1cf8e7d
happygirlll <lbm000314@gmail.com>
2025-06-11 16:43:12 +0900

    feat: nav바 메뉴 선택시 글자 색상 변경 및 호버 효과 추가

--- front/src/components/layout/NavBar.jsx
+++ front/src/components/layout/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import styles from './NavBar.module.css';
 import logo from '../../assets/main_logo.svg'
 import user_profile from '../../assets/user_profile.svg'
@@ -8,6 +8,7 @@ import {useOfficialCocktailContext} from "../../context/OfficialCocktailContext.
 export default function NavBar() {
     const navigate = useNavigate();
     const { resetList } = useOfficialCocktailContext();
+    const location = useLocation();
 
     const handleCocktailPage = () => {
         resetList();
@@ -23,13 +24,29 @@ export default function NavBar() {
         navigate('/mypage');
     };
 
+    const isUserRecipeActive = location.pathname === '/post';
+    const isOfficialRecipeActive = location.pathname === '/posts';
+
     return (
         <nav className={styles.nav}>
             <div className={styles.left}>
-                <img src={logo} alt="logo" className={styles.logo} onClick={handleCocktailPage}/>
+                <img src={logo} alt="logo" className={styles.logo} onClick={handleCocktailPage} />
                 <div className={styles.menu}>
-                    <span onClick={handleCocktailPage}>유저 레시피</span>
-                    <span onClick={handleOfficialPage}>공식 레시피</span>
+                    <span
+                        className={`${styles.menuItem} ${isUserRecipeActive ? styles.activeMenu : ''}`}
+                        onClick={handleCocktailPage}
+                    >
+  유저 레시피
+</span>
+
+                    <span
+                        className={`${styles.menuItem} ${isOfficialRecipeActive ? styles.activeMenu : ''}`}
+                        onClick={handleOfficialPage}
+                    >
+  공식 레시피
+</span>
+
+
                 </div>
             </div>
             <div className={styles.profile}>
@@ -37,7 +54,6 @@ export default function NavBar() {
                 <div className={styles.username}>
                     <span onClick={handleMyPage}>사용자</span>
                 </div>
-
             </div>
         </nav>
     );
